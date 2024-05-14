08e392c3ed6c4030bd901faa5316704370f05c11
Krzysztof <laudanski.krzysztof86@gmail.com>
2024-05-14 19:17:34 +0200

    add axios

--- src/App.vue
+++ src/App.vue
@@ -21,41 +21,44 @@
 </template>
 
 <script>
-import BooksList from './components/BooksList'
-import BooksLengthMsg from './components/BooksLengthMsg'
-import BookForm from './components/BookForm'
-import BooksSummary from './components/BooksSummary'
+import axios from 'axios';
+import BooksList from './components/BooksList';
+import BooksLengthMsg from './components/BooksLengthMsg';
+import BookForm from './components/BookForm';
+import BooksSummary from './components/BooksSummary';
 
 export default {
   name: 'App',
-  data: () => ({
-    books: [
-      {
-        title: 'The Catcher in the Rye',
-        price: 20
-      },
-      {
-        title: 'Of Mice and Men',
-        price: 18
-      }
-    ]
-  }),
+  data() {
+    return {
+      books: []
+    };
+  },
   methods: {
-    // eslint-disable-next-line
     removeBook(index) {
-      this.books.splice(index, 1)
+      this.books.splice(index, 1);
     },
-    // eslint-disable-next-line
     addBook(book) {
-      this.books.push({ ...book })
+      this.books.push({ ...book });
+    },
+    async fetchBooks() {
+      try {
+        const response = await axios.get('https://api.itbook.store/1.0/new');
+        const booksData = response.data.books.slice(0, 3).map(book => ({
+          title: book.title,
+          price: parseFloat(book.price.replace('$', '')) || 0
+        }));
+        this.books = booksData;
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
     }
   },
   computed: {
-    // eslint-disable-next-line
     booksSum() {
-      const totalPrice = this.books.reduce((total, book) => total + book.price, 0)
-      const totalBooks = this.books.length
-      return { totalPrice, totalBooks }
+      const totalPrice = this.books.reduce((total, book) => total + book.price, 0);
+      const totalBooks = this.books.length;
+      return { totalPrice, totalBooks };
     }
   },
   components: {
@@ -63,8 +66,11 @@ export default {
     BooksLengthMsg,
     BookForm,
     BooksSummary
+  },
+  created() {
+    this.fetchBooks();
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -77,6 +83,7 @@ export default {
   &__heading {
     font-size: 3rem;
     text-align: center;
+
     span {
       color: #5a58da;
     }
