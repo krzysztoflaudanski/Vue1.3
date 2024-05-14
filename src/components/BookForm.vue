de0400500a7b8afdefad0340588c0ad6113db450
Krzysztof <laudanski.krzysztof86@gmail.com>
2024-05-14 18:53:59 +0200

    add styles

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -18,15 +18,65 @@ export default {
 </script>
 
 <template>
-<form @submit.prevent="handleSubmit">
+<form @submit.prevent="handleSubmit" class="form">
     <label>
         Title:
-        <input v-model="form.title" type="text" name="title">
+        <input v-model="form.title" type="text" name="title" class="form__input">
     </label>
     <label>
         price:
-        <input v-model="form.price" type="number" name="price">
+        <input v-model="form.price" type="number" name="price" class="form__input">
     </label>
-    <button>Add book</button>
+    <button className="form__btn">Add book</button>
 </form>
 </template>
+
+<style lang="scss" scoped>
+.form {
+    border-radius: 2rem;
+    margin: 4rem 1rem;
+    box-shadow: 0 20px 40px 0 rgba(107, 154, 212, .1);
+    padding: 2rem;
+    background: #fff;
+    position: relative;
+    overflow: hidden;
+    font-size: 1rem;
+    color: #637892;
+
+    &__input {
+        background: none;
+        border: none;
+        outline: none;
+        padding: .7rem .7rem .7rem .7rem;
+        color: #000;
+        font-weight: lighter;
+        font-family: inherit;
+        border-bottom: 1px solid #5a58da;
+        margin: .5rem;
+        opacity: .8;
+        width: 100%;
+        max-width: 20rem;
+        transition: .2s;
+        font-size: 1rem;
+    }
+
+    &__btn {
+        padding: 1rem 1.5rem;
+        border: none;
+        box-shadow: 0 10px 40px 0 rgba(76, 104, 239, .35), 0 0 20px 0 rgba(76, 104, 239, .05);
+        font-family: inherit;
+        text-transform: uppercase;
+        font-weight: 700;
+        min-width: 6rem;
+        text-align: center;
+        margin: 2rem auto;
+        display: table;
+        cursor: pointer;
+        transition: .2s;
+        background: #f2f8ff;
+        color: #5a58da;
+        border: 2px solid #5a58da;
+        border-radius: 0.5rem;
+    }
+}
+</style>
--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -1,9 +1,10 @@
 <template>
 <div>
-  <ul>
-    <li :key="index" v-for="(book, index) in books">
+  <ul class="books-list">
+    <li :key="index" v-for="(book, index) in books" class="books-list__item"
+      :class="{ 'books-list__item--promotion': book.price < 20 }">
       {{ book.title }}, {{ book.price }}
-      <button @click="removeBook(index)">Remove</button>
+      <button @click="removeBook(index)" class="books-list__btn">Remove</button>
     </li>
   </ul>
 </div>
@@ -24,3 +25,47 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.books-list {
+  font-size: 1rem;
+  list-style-type: none;
+  margin: 1rem 0;
+  padding: 1rem;
+  color: #637892;
+  font-weight: 700;
+
+  &__item {
+    &--promotion {
+      font-size: 1.5rem;
+      text-decoration: underline;
+    }
+
+    padding: 1rem 0;
+    border-bottom: .0625rem dashed rgba(0, 0, 0, .1);
+  }
+
+  &__btn {
+    color: #fff;
+    box-shadow: 0 10px 40px 0 rgba(76, 104, 239, .35), 0 0 20px 0 rgba(76, 104, 239, .05);
+    border-radius: .3rem;
+    border: none;
+    font-family: inherit;
+    text-transform: uppercase;
+    font-weight: 700;
+    min-width: 6rem;
+    text-align: center;
+    cursor: pointer;
+    transition: .2s;
+    background: #ea2027;
+    color: #fff;
+    padding: .5rem 1rem;
+    font-size: .6rem;
+    margin: 0 .5rem;
+
+    &:hover {
+      box-shadow: 0 20px 40px 0 rgba(76, 104, 239, .35);
+    }
+  }
+}
+</style>
